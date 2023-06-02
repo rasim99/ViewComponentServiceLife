e9bd7b934ffa25059f7f2d201b595a8febbd363c
rasim99 <rasimjm@code.edu.az>
2023-06-03 01:48:47 +0400

    add Footer ViewComponent

---
+++ ViewComponentServiceLifeTime/ViewComponentServiceLifeTime/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ViewComponentServiceLifeTime.DAL;
+
+namespace ViewComponentServiceLifeTime.ViewComponents
+{
+    public class FooterViewComponent :ViewComponent
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public FooterViewComponent(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var bio = _appDbContext.Bios.FirstOrDefault();
+            return View(await Task.FromResult(bio));
+        }
+    }
+}
